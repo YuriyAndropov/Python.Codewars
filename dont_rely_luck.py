08e855786ac42fe49d8aa992e6b4366402d047e1
YuriyAndropov <yuriy.andropov@live.com>
2016-11-09 13:00:00 +0100

    Create dont_rely_luck.py

---
+++ dont_rely_luck.py
@@ -0,0 +1,17 @@
+#Description:
+#The test fixture I use for this kata is pre-populated.
+#It will compare your guess to a random number generated in Ruby by:
+#(Kernel::rand() * 100 + 1).floor
+#In Javascript/CoffeeScript by:
+#Math.floor(Math.random() * 100 + 1)
+#In Python by:
+#randint(1,100)
+#You can pass by relying on luck or skill but try not to rely on luck.
+#"The power to define the situation is the ultimate power." - Jerry Rubin
+#Good luck!
+#https://www.codewars.com/kata/5268af3872b786f006000228
+
+from random import randint,seed
+seed(1)
+guess = randint(1,100)
+seed(1)
