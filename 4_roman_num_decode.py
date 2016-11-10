693bec781ec40e25186ca43755912e7d3d4c170b
Yuriy Andropov <yuriy.andropov@live.com>
2016-11-10 16:38:35 +0100

    Create 4_roman_num_decode.py

---
+++ 4_roman_num_decode.py
@@ -0,0 +1,21 @@
+#Description:
+#Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer.
+#You don't need to validate the form of the Roman numeral.
+#Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately,
+#starting with the leftmost digit and skipping any 0s.
+#So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII).
+#The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
+#https://www.codewars.com/kata/51b6249c4612257ac0000005
+
+def solution(roman):
+    num = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+    res = 0
+    last = 0
+    for i in roman[::-1]:
+        if num[i]>=last:
+            res+=num[i]
+        else:
+            res-=num[i]
+        last = num[i]
+    print res
+    return res 
