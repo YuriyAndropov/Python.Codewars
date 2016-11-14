94b34e32cc9899efe2bd03fcf32b64746f02b221
Yuriy Andropov <yuriy.andropov@live.com>
2016-11-14 11:48:06 +0100

    Update 4_observ_PIN.py
    refactor. Learned from another person solution

--- 4_observ_PIN.py
+++ 4_observ_PIN.py
@@ -17,32 +17,17 @@
 #Detective, we count on you!
 #https://www.codewars.com/kata/5263c6999e0f40dee200059d
 
-import re
 from itertools import product
 def get_pins(observed):
     keys = {
-        0: [0, 8],
-        1: [1, 2, 4],
-        2: [1, 2, 3, 5],
-        3: [2, 3, 6],
-        4: [1, 4, 5, 7],
-        5: [2, 4, 5, 6, 8],
-        6: [3, 5, 6, 9],
-        7: [4, 7, 8],
-        8: [0, 5, 7, 8, 9],
-        9: [6, 8, 9]
-    }
-    result = []
-    out = []
-    fin = []
-    if len(observed) == 1:
-        return "".join(map(str,keys[int(observed)]))
-    else:
-        for i in range(0,len(observed)):
-            result.append(keys[int(observed[i])])
-    for e in product(*result):
-        out.append(str(e))
-    for i in out:
-        strip = re.sub('[^0-9]','', i)
-        fin.append(strip)
-    return fin    
+       '1': ('1', '2', '4'), 
+       '2': ('1', '2', '3', '5'), 
+       '3': ('2', '3', '6'), 
+       '4': ('1', '4', '5', '7'), 
+       '5': ('2', '4', '5', '6', '8'), 
+       '6': ('5', '6', '9', '3'), 
+       '7': ('4', '7', '8'), 
+       '8': ('7', '5', '8', '9', '0'), 
+       '9': ('6', '8', '9'), '0': ('0', '8')}
+    return [''.join(x) for x in product(*(keys[y] for y in observed))]
+    
